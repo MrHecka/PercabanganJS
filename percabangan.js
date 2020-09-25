199fb81863656369f2b8bf84d419893e57a2bb12
Muhammad Mamad <muhammadmamad41@gmail.com>
2020-09-25 18:41:13 +0700

    Percabangan.JS

--- percabangan.js
+++ percabangan.js
@@ -1,4 +1,4 @@
-var angka = prompt('Masukkan Angka!te  afwasttt');
+var angka = prompt('Masukkan Angka!');
 
 for (var i = 1; i <= 21; i++) {
     for (var j = 1; j <= i; j++) {
@@ -7,7 +7,7 @@ for (var i = 1; i <= 21; i++) {
     for (var k = i; k <= angka; k++) {
         document.write("*");
         for (var j = i; j <= i; j++)
-            document.write("=");
+            document.write("=====");
     }
 
     document.write("<br>");
