a3f2bd33a6eaacddf6dab14ebefe1cb1f78a3e33
Muhammad Mamad <muhammadmamad41@gmail.com>
2020-09-25 19:40:31 +0700

    Tugas DTP

--- percabangan.js
+++ percabangan.js
@@ -1,19 +1,14 @@
-var k = prompt('Masukkan Angka!');
+var angka = prompt('Masukkan Angka!');
 
-for (var i = 5; i >= 1; i--) {
-    var l = i;
+for (var i = 1; i <= 21; i++) {
     for (var j = 1; j <= i; j++) {
-        if (j > k) {
-            if (l % 2 == 0) {
-                document.write("=");
-            } else {
-                document.write("*");
-            }
-        } else {
-            document.write("&nbsp;&nbsp;");
-        }
-        l++;
+        document.write("&nbsp&nbsp");
     }
+    for (var k = i; k <= angka; k++) {
+        document.write("*");
+        for (var j = i; j <= i; j++)
+            document.write("=");
+    }
+
     document.write("<br>");
-    k++;
 }
